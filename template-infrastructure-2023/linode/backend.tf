ef3ce703c310c9aa5b66600fb63f35fa6b05600c
zcui <zhanming.cui@asavie.com>
2023-11-06 12:10:22 +0000

    Using Terraform Cloud for state management

--- template-infrastructure-2023/linode/backend.tf
+++ template-infrastructure-2023/linode/backend.tf
@@ -1,11 +1,8 @@
 terraform {
-  backend "s3" {
-    skip_credentials_validation = true
-    skip_region_validation      = true
-    endpoint                    = "eu-central-1.linodeobjects.com"
-    profile                     = "linode-zcui"
-    bucket                      = "infra-tf-state-20231023"
-    key                         = "infra/state.json"
-    region                      = "eu-central-1"
+  cloud {
+    organization = "cuizhanming-com"
+    workspaces {
+      tags = ["terraform-state-linode-dev"]
+    }
   }
 }
--- template-infrastructure-2023/linode/labs/dev/init-state/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "linode_api_token" {
-  description = "Linode API Personal Access Token. (required) Can load from ENV or config file (~/.config/linode)"
-}
