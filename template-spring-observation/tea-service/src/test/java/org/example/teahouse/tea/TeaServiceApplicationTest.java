7747cf63f5a938ae982d7beecd56b482632ac426
cuizhanming <1814462+cuizhanming@users.noreply.github.com>
2025-06-09 11:50:25 +0100

    remove template-spring-observation

--- template-spring-observation/tea-service/src/test/java/org/example/teahouse/tea/TeaServiceApplicationTest.java
+++
@@ -1,9 +0,0 @@
-package org.example.teahouse.tea;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = TeaServiceApplication.class)
-class TeaServiceApplicationTest {
-    @Test void contextLoads() {}
-}
--- template-spring-observation/water-service/water-api/src/main/java/org/example/teahouse/water/api/WaterModel.java
+++
@@ -1,9 +0,0 @@
-package org.example.teahouse.water.api;
-
-import java.util.UUID;
-
-public interface WaterModel {
-    UUID getId();
-    String getSize();
-    String getAmount();
-}
