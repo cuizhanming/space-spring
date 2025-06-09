471fa0da0dfecfc514d28237fbfc9f2c18ba9845
cuizhanming <1814462+cuizhanming@users.noreply.github.com>
2025-06-09 12:45:02 +0100

    Remove kotlin modules

--- template-kotlin-spring-reactive/settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "template-kotlin-spring-reactive"
-pluginManagement {
-    val kotlinVersion: String by extra
-
-    plugins {
-        kotlin("jvm") version kotlinVersion
-        kotlin("plugin.spring") version kotlinVersion
-    }
-}
--- template-kotlin-spring-security/src/main/resources/db/migration/postgresql/schema.sql
+++
@@ -1,7 +0,0 @@
-create table users (
-   id UUID DEFAULT gen_random_uuid() not null,
-   email varchar(100) not null UNIQUE,
-   password varchar(100) not null,
-   name varchar(100) not null,
-   primary key (id)
-);
